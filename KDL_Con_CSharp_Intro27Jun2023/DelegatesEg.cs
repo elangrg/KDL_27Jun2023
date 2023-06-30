8ea8f00c61ead19bbf46f624ac660639cffdb8ec
elangrg <45620769+elangrg@users.noreply.github.com>
2023-06-30 10:35:44 +0530

    Delegate

---
+++ KDL_Con_CSharp_Intro27Jun2023/DelegatesEg.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KDL_Con_CSharp_Intro27Jun2023
+{
+
+    delegate int dlgDemo(int a, int b);
+
+
+
+    internal class DelegatesEg
+    {
+
+        static void Main()
+        {
+
+            // Syntax 1.0 
+            dlgDemo fptr = new dlgDemo(Add);
+
+
+            // Syntax 2.0 DTI 
+             fptr += Multi;
+
+
+
+            Console.WriteLine(fptr(100, 200));
+
+            
+
+        
+        
+        }
+
+        static int Add(int i, int j)
+        {
+            return i + j;
+        }
+
+   static int Multi(int k, int l)
+        {
+            return k * l;
+        }
+
+    }
+}
