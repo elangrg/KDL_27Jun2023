be164049dedea7284799d46f740e2bd6c7359f7c
elangrg <45620769+elangrg@users.noreply.github.com>
2023-07-03 14:57:54 +0530

    t2

--- KDL_Con_CSharp_Intro27Jun2023/ThreadingEg.cs
+++ KDL_Con_CSharp_Intro27Jun2023/ThreadingEg.cs
@@ -14,16 +14,22 @@ namespace KDL_Con_CSharp_Intro27Jun2023
             Worker worker = new Worker();
 
             Thread th = new Thread(worker.DoWork);
-            th.IsBackground= true;  
+            th.Name = "S1";
             th.Start();
 
 
-            for (int cnt = 0; cnt < 100; cnt++)
-            {
-                Console.WriteLine($"Managed Thread Id : {Thread.CurrentThread.ManagedThreadId} , Thread Name :{Thread.CurrentThread.Name} , Inc Value : {cnt}  ");
-                Thread.Sleep(100);
 
-            }
+            Thread th1 = new Thread(worker.DoAnotherWork);
+            th1.Name = "S2";
+            
+            th1.Start();
+
+            //for (int cnt = 0; cnt < 100; cnt++)
+            //{
+            //    Console.WriteLine($"Managed Thread Id : {Thread.CurrentThread.ManagedThreadId} , Thread Name :{Thread.CurrentThread.Name} , Inc Value : {cnt}  ");
+            //    Thread.Sleep(100);
+
+            //}
             Console.WriteLine("Press any key to continue....");
 
             Console.ReadKey();
@@ -40,18 +46,39 @@ namespace KDL_Con_CSharp_Intro27Jun2023
         {
 
 
-            for (int i = 0; i < 100; i++)
+            lock (this)
             {
-                inc++;
+                for (int i = 0; i < 100; i++)
+                {
+                    inc++;
 
-                Console.WriteLine($"Managed Thread Id : {Thread.CurrentThread.ManagedThreadId} , Thread Name :{Thread.CurrentThread.Name} , Inc Value : {inc}  ");
-                Thread.Sleep(300);
+                    Console.WriteLine($"Managed Thread Id : {Thread.CurrentThread.ManagedThreadId} , Thread Name :{Thread.CurrentThread.Name} , Inc Value : {inc}  ");
+                    Thread.Sleep(300);
 
+                } 
             }
         
         }
 
+  public void DoAnotherWork()
+        {
 
+            lock (this)
+            {
+
+
+
+                for (int i = 0; i < 100; i++)
+                {
+                    inc++;
+
+                    Console.WriteLine($"Managed Thread Id : {Thread.CurrentThread.ManagedThreadId} , Thread Name :{Thread.CurrentThread.Name} , Inc Value : {inc}  ");
+                    Thread.Sleep(300);
+
+                } 
+            }
+        
+        }
 
 
     }
