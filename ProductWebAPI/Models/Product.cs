0b9bf75ab68521a53cd6f13bcefdc081314b0686
elangrg <45620769+elangrg@users.noreply.github.com>
2023-07-04 17:18:34 +0530

    new prj

---
+++ ProductWebAPI/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProductWebAPI.Models
+{
+    public partial class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = null!;
+        public int Qty { get; set; }
+        public string Category { get; set; } = null!;
+    }
+}
---
+++ ProductWebAPI/Program.cs
@@ -0,0 +1,27 @@
+namespace ProductWebAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddScoped<Models.KNDProductDbContext>();
+
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
